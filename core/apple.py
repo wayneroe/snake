6cd5dd5a8ffd655f39cdcb818375148b4b1bda3d
Abrogans <wayne.roettger@bluewin.ch>
2021-03-10 23:46:42 +0100

    Add sound and an icon

--- core/apple.py
+++ core/apple.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from random import randint
 
 import pygame as pg
@@ -9,13 +10,21 @@ class Apple(object):
     def __init__(self, tiles, snake):
         self.snake = snake
         self.tiles = tiles
-        self.coordinate = Point(randint(0, tiles.map_size - 1), randint(0, tiles.map_size - 1))
+        self.coordinate = self.get_point_not_in_snake()
+
+        sound_folder = Path("../sounds/")
+        self.eating_sound = pg.mixer.Sound(str(sound_folder / "eat.ogg"))
+
+    def get_point_not_in_snake(self):
+        while True:
+            coordinate = Point(randint(0, self.tiles.map_size - 1), randint(0, self.tiles.map_size - 1))
+            if coordinate not in self.snake.body_coordinates:
+                return coordinate
 
     def update(self, dt):
-        print(f"Apple:\n{self.coordinate} Snake:\n{self.snake.body_coordinates}")
         if self.coordinate in self.snake.body_coordinates:
-            print("Touched apple!")
             self.snake.ate_apple = True
+            self.eating_sound.play()
             self.coordinate = Point(randint(0, self.tiles.map_size - 1), randint(0, self.tiles.map_size - 1))
 
     def draw(self, surface):
--- core/main.py
+++ core/main.py
@@ -1,4 +1,5 @@
 import sys
+from pathlib import Path
 
 import pygame as pg
 
@@ -7,7 +8,13 @@ from core.states import game, splash, menu, over
 
 if __name__ == "__main__":
     pg.init()
+    pg.mixer.init()
     screen = pg.display.set_mode((720, 720), pg.RESIZABLE)
+    images_folder = Path("../images/")
+    icon_path = str(images_folder / "snake-icon.png")
+    icon = pg.image.load(icon_path)
+    pg.display.set_icon(icon)
+    pg.display.set_caption("Snake")
     states = {"GAME": game.Game(),
               "SPLASH": splash.Splash(),
               "MENU": menu.Menu(),
