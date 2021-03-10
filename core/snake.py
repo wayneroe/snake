148962ad90ac3ba9dee729b555d4893a1f17bd38
Abrogans <wayne.roettger@bluewin.ch>
2021-03-10 23:45:54 +0100

    Refactor snake class

--- core/snake.py
+++ core/snake.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pygame as pg
 
 from core.tools import Point, Directions
@@ -6,48 +8,56 @@ from core.tools import Point, Directions
 class Snake(object):
     def __init__(self, game, snake_speed, tiles):
         self.game = game
-        self.screen_rect = pg.display.get_surface().get_rect()
-        self.screen_height = self.screen_rect.height
-        self.screen_width = self.screen_rect.width
+        self.tiles = tiles
+        self.snake_speed = snake_speed
+        self.score = 0
 
         middle = tiles.map_size // 2
+
         self.ate_apple = False
+        sound_folder = Path("../sounds/")
+        self.eating_sound = pg.mixer.Sound(str(sound_folder / "gameOver.mp3"))
+
         self.body_coordinates = list([Point(middle, middle)])
 
-        self.tiles = tiles
-
-        self.image = pg.Surface((self.screen_width, self.screen_height))
-
-        self.snake_speed = snake_speed
         self.direction = Directions.RIGHT
+        self.old_direction = Directions.RIGHT
         # How far the snake is on the last block
-        self.block_progress = 0
+        self.progress = 0
         self.last_dt = 0
 
     def update(self, dt):
-        self.block_progress = self.block_progress + dt * self.snake_speed
-        if self.block_progress > 1:
-            new_body_part = self.body_coordinates[-1].walk(self.direction)
-            if not new_body_part.in_range(self.tiles.map_size):
-                return False
-            if self.ate_apple:
-                self.body_coordinates.append(new_body_part)
-                self.ate_apple = False
-            else:
-                if len(self.body_coordinates) > 1:
-                    self.body_coordinates.append(new_body_part)
-                    self.body_coordinates.pop(0)
-                else:
-                    self.body_coordinates[-1] = new_body_part
-            self.block_progress = self.block_progress - 1
-        return True
+        self.progress = self.progress + (dt * self.snake_speed)
+        if self.progress > 1:
+            self.move()
+            self.progress = self.progress - 1
+
+    def move(self):
+        head = self.body_coordinates[-1]
+        new_head = head.walk(self.direction)
+        self.old_direction = self.direction
+
+        if not new_head.in_range(self.tiles.map_size) or new_head in self.body_coordinates:
+            self.game.done = True
+            self.game.next_state = "GAME_OVER"
+            self.eating_sound.play()
+            return
+
+        self.body_coordinates.append(new_head)
+        if self.ate_apple:
+            self.score += 1
+            self.ate_apple = False
+        else:
+            self.body_coordinates.pop(0)
 
     def draw(self, screen):
         for block in self.body_coordinates:
             pg.draw.rect(screen, pg.Color("red"), self.tiles.get_position(block))
 
-    def direction_allowed(self, direction):
-        if (self.direction.value[0] + direction.value[0] == 0) and (self.direction.value[1] + direction.value[1] == 0):
-            return False
+    def change_direction(self, direction):
+        if (self.old_direction.value[0] + direction.value[0] == 0) \
+                and (self.old_direction.value[1] + direction.value[1] == 0):
+            # Snake tries to move in the direction it came from
+            return
         else:
-            return True
+            self.direction = direction
