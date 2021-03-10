d35a6f17276dbd096f278bd32da81f2be3740797
Abrogans <wayne.roettger@bluewin.ch>
2021-03-10 23:47:16 +0100

    Shorten calculation of offsets

--- core/tiles.py
+++ core/tiles.py
@@ -6,57 +6,48 @@ class Tiles(object):
     def __init__(self, map_size):
         self.map_size = map_size
 
-        self.tile_rects = np.empty((map_size, map_size), pg.Rect)
         self.screen_rect = pg.display.get_surface().get_rect()
         self.screen_width = self.screen_rect.width
         self.screen_height = self.screen_rect.height
 
         self.tile_size = min(self.screen_width, self.screen_height) // self.map_size
 
-        self.x_offset, self.y_offset = self.calculate_offsets()
-
-        for i in range(self.map_size):
-            for j in range(self.map_size):
-                tile_rect = [self.x_offset + i * self.tile_size, self.y_offset + j * self.tile_size,
-                             self.tile_size, self.tile_size]
-                self.tile_rects[i][j] = tile_rect
+        x_offset, y_offset = self.calculate_offsets()
+        self.tile_rects = self.calculate_rects(x_offset, y_offset)
 
         self.bright_green = pg.Color("GreenYellow")
         self.dark_green = pg.Color("LawnGreen")
         self.font = pg.font.SysFont('Arial', 35)
 
     def calculate_offsets(self):
-        x_offset = 0
-        y_offset = 0
+        tiles_size = self.map_size * self.tile_size
 
-        map_middle = (self.map_size * self.tile_size) // 2
-
-        if self.screen_width != self.screen_height:
-            if self.screen_width > self.screen_height:
-                x_offset = self.screen_width - map_middle
-            else:
-                y_offset = self.screen_height - map_middle
-        print(x_offset, y_offset)
+        x_offset = (self.screen_width - tiles_size) / 2
+        y_offset = (self.screen_height - tiles_size) / 2
 
         return x_offset, y_offset
 
-    def draw(self, surface, score):
-        surface.fill(pg.Color(87, 138, 52))
+    def calculate_rects(self, x_offset, y_offset):
+        tile_rects = np.empty((self.map_size, self.map_size), pg.Rect)
 
         for i in range(self.map_size):
             for j in range(self.map_size):
-                tile_rect = [self.x_offset + i * self.tile_size, self.y_offset + j * self.tile_size,
+                tile_rect = [x_offset + i * self.tile_size, y_offset + j * self.tile_size,
                              self.tile_size, self.tile_size]
+                tile_rects[i][j] = tile_rect
 
-                self.tile_rects[i][j] = tile_rect
+        return tile_rects
 
+    def draw(self, surface, score):
+        for i in range(self.map_size):
+            for j in range(self.map_size):
                 if (j + i) % 2 == 0:
-                    pg.draw.rect(surface, self.bright_green, tile_rect)
+                    pg.draw.rect(surface, self.bright_green, self.tile_rects[i][j])
                 else:
-                    pg.draw.rect(surface, self.dark_green, tile_rect)
+                    pg.draw.rect(surface, self.dark_green, self.tile_rects[i][j])
 
         score_text = self.font.render(f"Score {score}", True, pg.Color("Black"))
-        surface.blit(score_text, (self.x_offset, 0))
+        surface.blit(score_text, (0, 0))
 
     def update_window_size(self, width, height):
         self.screen_width = width
@@ -64,7 +55,8 @@ class Tiles(object):
 
         self.tile_size = min(self.screen_width, self.screen_height) // self.map_size
 
-        self.x_offset, self.y_offset = self.calculate_offsets()
+        x_offset, y_offset = self.calculate_offsets()
+        self.tile_rects = self.calculate_rects(x_offset, y_offset)
 
     def get_position(self, coordinates):
         return self.tile_rects[coordinates.x][coordinates.y]
